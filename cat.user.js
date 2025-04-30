d9b33fadb4d8779d5df3bf7c6a566eab182ff5b1
Anton Tkach <dekerter@gmail.com>
2025-04-30 04:07:03 +0300

    Fix +1h +2.5 buttons sometimes not displaying

--- cat.user.js
+++ cat.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Customer Admin Toolkit
 // @namespace    http://tampermonkey.net/
-// @version      0.2.1.1
+// @version      0.2.2
 // @description  Add QoL improvement to CRM
 // @author       Anton Tkach <anton.tkach.dev@gmail.com>
 // @include      https://*.kommo.com/todo/calendar/week/*
@@ -47,16 +47,6 @@
       });
   }
 
-  const observer = new MutationObserver(() => {
-      changeEventColors();
-  });
-
-  observer.observe(document.body, {
-      childList: true,
-      subtree: true
-  });
-  changeEventColors();
-
       // ================ +1h, +2.5h section =============
       /**
        * Converts sting date time to Date object
@@ -92,43 +82,71 @@
         datetime.setMinutes(datetime.getMinutes() + (hours % 1) * 60);  // Add fractional minutes
         return formatDatetime(datetime)
     }
-    
-    const dateSource = document.querySelector('[data-id="770966"]');
-    const dateTargetInput = document.querySelector('[data-id="770968"] input')
-    dateSource.querySelector('.linked-form__field__value').style.setProperty('max-width', '150px');
 
-    const button1h = document.createElement('div');
-    button1h.textContent = '+1h';
-    button1h.classList.add('qol-button');
+    function addQolButtons() {
+        const dateSource = document.querySelector('[data-id="770966"]');
+        const dateTargetInput = document.querySelector('[data-id="770968"] input')
 
-    const button2_5h = document.createElement('div');
-    button2_5h.textContent = '+2.5h';
-    button2_5h.classList.add('qol-button');
+        if (dateSource && dateTargetInput) {
+            if (dateSource.querySelector('.qol-button')) return;
+            dateSource.querySelector('.linked-form__field__value').style.setProperty('max-width', '150px');
 
-    const setupHourAdder = (hoursToAdd) => {
-        return (event) => {
-            event.preventDefault(); // Prevent the form submission to ajax
-            event.stopPropagation(); // Stop the event from propagating up the DOM
-        
-            const inputElement = dateSource.querySelector('input');
-            if (inputElement && inputElement.value) {
-                const targetString = addHours(inputElement.value, hoursToAdd);
-            
-                // Add timeout to reduce flakiness
-                document.body.style.cursor = 'progress';
-                // setTimeout(() => {
-                    dateTargetInput.focus();
-                    dateTargetInput.value = targetString;
-                    dateTargetInput.blur();
-                    // document.body.style.cursor = 'default';
-                // }, 500);
-            }
+            const button1h = document.createElement('div');
+            button1h.textContent = '+1h';
+            button1h.classList.add('qol-button');
+
+            const button2_5h = document.createElement('div');
+            button2_5h.textContent = '+2.5h';
+            button2_5h.classList.add('qol-button');
+
+            const setupHourAdder = (hoursToAdd) => {
+                return (event) => {
+                    event.preventDefault();  // Prevent the form submission to ajax
+                    event.stopPropagation(); // Stop the event from propagating up the DOM
+
+                    const inputElement = dateSource.querySelector('input');
+                    if (inputElement && inputElement.value) {
+                        const targetString = addHours(inputElement.value, hoursToAdd);
+
+                        // Add timeout to reduce flakyness
+                        document.body.style.cursor = 'progress';
+                        // setTimeout(() => {
+                        dateTargetInput.focus();
+                        dateTargetInput.value = targetString;
+                        dateTargetInput.blur();
+                        // document.body.style.cursor = 'default';
+                        // }, 500);
+                    }
+                };
+            };
+
+            button1h.addEventListener('click', setupHourAdder(1));     // Adds 1 hour
+            button2_5h.addEventListener('click', setupHourAdder(2.5)); // Adds 2.5 hours
+
+            dateSource.appendChild(button1h);
+            dateSource.appendChild(button2_5h);
+        }
+    }
+
+    let debounceTimeout;
+    const debounce = (func, delay) => {
+        return (...args) => {
+            clearTimeout(debounceTimeout);
+            debounceTimeout = setTimeout(() => {
+                func(...args);
+            }, delay);
         };
     };
-    
-    button1h.addEventListener('click', setupHourAdder(1));     // Adds 1 hour
-    button2_5h.addEventListener('click', setupHourAdder(2.5)); // Adds 2.5 hours
-    
-    dateSource.appendChild(button1h);
-    dateSource.appendChild(button2_5h);
+
+    const observer = new MutationObserver(debounce(() => {
+        changeEventColors();
+        addQolButtons();
+    }, 50));
+
+    observer.observe(document.body, {
+        childList: true,
+        subtree: true
+    });
+    changeEventColors();
+    addQolButtons();
 })();
